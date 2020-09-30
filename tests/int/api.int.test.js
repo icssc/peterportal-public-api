405751f68a5db04f94622b82266b63040af81156
ramanxg <ramanxg@gmail.com>
2020-09-30 13:07:32 -0700

    updated tests

--- tests/int/api.int.test.js
+++ tests/int/api.int.test.js
@@ -7,11 +7,11 @@ describe('GET /courses/all', () => {
     it('returns a json of all the courses on the catalogue', async () => request(app)
     .get('/rest/v1/courses/all')
     .set('Accept', 'application/json')
-    .expect('Content-Type', '/json/')
+    .expect('Content-Type', /json/)
     .expect(200)
     .then((response) => {
         expect(response.body['I&CSCI46']).not.toBeUndefined();
-        expect(response.body['I&CSCI46']['id'], 'I&C SCI 46');
+        expect(response.body['I&CSCI46']['id']).toEqual('I&C SCI 46');
     }));
 });
 
@@ -20,11 +20,11 @@ describe('GET /courses/I&CSCI33', () => {
     it('returns a json of the specific course on the catalogue', async () => request(app)
     .get('/rest/v1/courses/I&CSCI33')
     .set('Accept', 'application/json')
-    .expect('Content-Type', '/json/')
+    .expect('Content-Type', /json/)
     .expect(200)
     .then((response) => {
-        expect(response.body['id'], 'I&C SCI 33');
-        expect(response.body['name'], 'Intermediate Programming');
-        expect(typeof response.body['dependencies'], 'array');
+        expect(response.body['id']).toEqual('I&C SCI 33');
+        expect(response.body['name']).toEqual('Intermediate Programming');
+        expect(Array.isArray(response.body['dependencies'])).toBeTruthy();
     }));
 });
