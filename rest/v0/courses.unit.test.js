82e1292f75cd550774c760622da120c6ebcb8473
Raman <ramanxg@gmail.com>
2020-11-01 17:32:26 -0800

    Created new tests for instructor

--- rest/v0/courses.unit.test.js
+++ rest/v0/courses.unit.test.js
@@ -6,10 +6,13 @@ describe('Get all Courses', () => {
             const allCourses = getAllCourses();
             expect(allCourses).not.toBeNull() 
             expect(typeof allCourses).toBe("object");
-            expect(allCourses).toMatchObject({
-                "I&CSCI46": expect.anything(),
-                "I&CSCI33": expect.anything()
-            });
+            expect(Array.isArray(allCourses)).toBe(true);
+            expect(allCourses).toContainEqual(
+                expect.objectContaining({"number": "46", "department": "I&C SCI"})
+            );
+            expect(allCourses).toContainEqual(
+                expect.objectContaining({"number": "134E", "department": "ART HIS"})
+            );
         });
     });
 });
@@ -22,30 +25,19 @@ describe('Get specific Courses', () => {
             expect(typeof course).toBe("object");
             expect(course).toMatchObject({
                 "id": "I&C SCI 46",
-                "id_department": "I&C SCI",
-                "id_number": "46",
-                "id_school": "Donald Bren School of Information and Computer Sciences",
-                "name": "Data Structure Implementation and Analysis",
+                "department": "I&C SCI",
+                "school": "Donald Bren School of Information and Computer Sciences",
+                "title": "Data Structure Implementation and Analysis",
                 "course_level": "Lower Division (1-99)",
-                "dept_alias": ["ICS"],
+                "department_alias": ["ICS"],
                 "units": [4, 4],
                 "description": "Focuses on implementation and mathematical analysis of fundamental data structures and algorithms. Covers storage allocation and memory management techniques.",
-                "department": "Information and Computer Science",
-                "professorHistory": ["sgagomas", "klefstad", "pattis", "thornton"],
-                "prerequisiteJSON": "{\"OR\":[\"CSE 45C\",\"I&C SCI 45C\"]}",
-                "prerequisiteList": ["I&C SCI 45C", "CSE 45C"],
-                "prerequisite": "( CSE 45C OR I&C SCI 45C ) AND ( NO REPEATS ALLOWED IF GRADE = C OR BETTER ) AND ( SCHOOL OF I&C SCI ONLY OR COMPUTER SCI & ENGR MAJORS ONLY )",
-                "dependencies": ["COMPSCI 111", "COMPSCI 116", "COMPSCI 117", "COMPSCI 141", "COMPSCI 143A", "COMPSCI 145", "COMPSCI 146", "COMPSCI 161", "COMPSCI 162", "COMPSCI 164", "COMPSCI 171", "COMPSCI 190", "COMPSCI 216", "COMPSCI 217", "COMPSCI 261", "EECS 111", "IN4MATX 101", "IN4MATX 115", "IN4MATX 122", "LSCI 102"],
-                "repeatability": "",
-                "grading option": "",
-                "concurrent": "",
-                "same as": "",
-                "restriction": "",
-                "overlaps": "",
-                "corequisite": "",
-                "ge_types": ["GE Vb: Formal Reasoning"],
-                "ge_string": "(Vb)",
-                "terms": ["2020 Fall", "2019 Spring"]
+                "professor_history": expect.arrayContaining(["sgagomas", "klefstad", "pattis", "thornton", "mikes"]),
+                "prerequisite_tree": "{\"OR\":[\"CSE 45C\",\"I&C SCI 45C\"]}",
+                "prerequisite_list": expect.arrayContaining(["I&C SCI 45C", "CSE 45C"]),
+                "prerequisite_text": "( CSE 45C OR I&C SCI 45C ) AND ( SCHOOL OF I&C SCI ONLY OR COMPUTER SCI & ENGR MAJORS ONLY )",
+                "dependencies": ["COMPSCI 111", "COMPSCI 116", "COMPSCI 117", "COMPSCI 122D", "COMPSCI 141", "COMPSCI 143A", "COMPSCI 145", "COMPSCI 146", "COMPSCI 161", "COMPSCI 162", "COMPSCI 164", "COMPSCI 171", "COMPSCI 190", "COMPSCI 216", "COMPSCI 217", "COMPSCI 261", "EECS 111", "IN4MATX 101", "IN4MATX 115", "IN4MATX 122", "LSCI 102"],
+                "terms": ["2020 Winter","2020 Fall", "2019 Fall","2019 Winter","2019 Spring","2018 Fall","2018 Winter","2018 Spring","2018 Summer1","2018 Summer10wk","2017 Fall","2017 Winter","2017 Spring"]
             });
         });
     });
