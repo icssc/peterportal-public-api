9966bf3cf031458ff42a77cf0251b7730a494d55
pranavmreddy <pranavmr@uci.edu>
2021-01-29 01:44:50 -0800

    Schedule endpoint is working

--- rest/v0/schedule.js
+++ rest/v0/schedule.js
@@ -6,14 +6,9 @@ var {callWebSocAPI} = require('websoc-api');
 
 router.get("/soc", function (req, res, next) {
     callWebSocAPI(req.query).then((val) => {
-        // The following console.log works, but setting the response does not
-        // Something occurs with the credentials
-        // console.log('result', val);
         res.json(val)
-        
     }).catch((err) => {
-        console.log('your error: ', err);
-        // res.status(404).json(createErrorJSON(404, "Bad Request: Invalid parameter", "Unable to complete websoc-api query"));
+        res.status(404).json(createErrorJSON(404, "Bad Request: Invalid parameter", "Unable to complete websoc-api query"));
     })
 })
 
