4151fa03e05c64e14832e48be95051d0ae6aa7d5
ramanxg <ramanxg@gmail.com>
2021-02-01 09:21:28 -0700

    Fixed referer bug for playground

--- keys/apiKeyAuth.js
+++ keys/apiKeyAuth.js
@@ -17,10 +17,13 @@ const {
 } = faunadb.query;
 
 let apiKeyAuth = (req, res, next) => {
+   
     if (process.env.NODE_ENV == "development") {
         next();
-    }
-    if (!req.headers["x-api-key"]) {
+    } else if (req.headers['referer'] && req.headers['referer'].includes('graphql-playground')) {
+        next();
+    } 
+    else if (!req.headers["x-api-key"]) {
         res.status(401).json(createErrorJSON(401, "No credentials sent.", "No credentials were found in the header of the request. See documentation for more info."));
     } else {
         // add check to make sure key is active
