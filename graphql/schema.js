83d5641306f71743b8b0015bf846429f9744267a
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-03-01 02:23:34 -0800

    Add descriptions to schedule query args

--- graphql/schema.js
+++ graphql/schema.js
@@ -315,27 +315,26 @@ const queryType = new GraphQLObjectType({
     schedule: {
       type: GraphQLList(courseType),
 
-      // TODO: Add descriptions and note required fields.
       args: {
-        year: { type: GraphQLFloat },
-        quarter: { type: GraphQLString },
-        ge: { type: GraphQLString },
-        department: { type: GraphQLString },
-        course_number: { type: GraphQLString} ,
-        division: { type: GraphQLString },
-        section_codes: { type: GraphQLString },
-        instructor: { type: GraphQLString },
-        course_title: { type: GraphQLString },
-        section_type: { type: GraphQLString },
-        units: { type: GraphQLString },
-        days: { type: GraphQLString },
-        start_time: { type: GraphQLString },
-        end_time: { type: GraphQLString },
-        max_capacity: { type: GraphQLString },
-        full_courses: { type: GraphQLString },
-        cancelled_courses: { type: GraphQLString },
-        building: { type: GraphQLString },
-        room: { type: GraphQLString }
+        year: { type: GraphQLFloat, description: "Year of the term. Required." },
+        quarter: { type: GraphQLString, description: "Quarter of the term. ['Fall'|'Winter'|'Spring'|'Summer1'|'Summer2'|'Summer10wk']. Required." },
+        ge: { type: GraphQLString, description: "GE type. ['ANY'|'GE-1A'|'GE-1B'|'GE-2'|'GE-3'|'GE-4'|'GE-5A'|'GE-5B'|'GE-6'|'GE-7'|'GE-8']." },
+        department: { type: GraphQLString, description: "Department Code." },
+        course_number: { type: GraphQLString, description: "Course number or range. Ex: '32A' or '31-33'." },
+        division: { type: GraphQLString, description: "Division level of a course. ['ALL'|'LowerDiv'|'UpperDiv'|'Graduate']. Default: 'ALL'." },
+        section_codes: { type: GraphQLString, description: "5-digit course code or range. Ex: '36531' or '36520-36536'." },
+        instructor: { type: GraphQLString, description: "Instructor last name or part of last name." },
+        course_title: { type: GraphQLString, description: "Title of a course." },
+        section_type: { type: GraphQLString, description: "Type of section. ['ALL'|'ACT'|'COL'|'DIS'|'FLD'|'LAB'|'LEC'|'QIZ'|'RES'|'SEM'|'STU'|'TAP'|'TUT']. Default: 'ALL'." },
+        units: { type: GraphQLString, description: "Unit count of a course. Ex: '4' or '1.3'. Use 'VAR' to look for variable unit classes." },
+        days: { type: GraphQLString, description: "Days that a course is offered. Any combination of ['Su'|'M'|'T'|'W'|'Th'|'F'|'Sa']. Ex: 'MWF'." },
+        start_time: { type: GraphQLString, description: "Start time of a couse in 12 hour format. Ex: '10:00AM' or '5:00PM'" },
+        end_time: { type: GraphQLString, description: "End time of a couse in 12 hour format. Ex: '10:00AM' or '5:00PM'" },
+        max_capacity: { type: GraphQLString, description: "Maximum enrollment capacity of a choice. Specify a " },
+        full_courses: { type: GraphQLString, description: "Status of a course's enrollment state. ['ANY'|'SkipFullWaitlist'|'FullOnly'|'OverEnrolled']. Default: 'ANY'." },
+        cancelled_courses: { type: GraphQLString, description: "Indicate whether to ['Exclude'|'Include'|'Only'] cancelled courses. Default: 'EXCLUDE'." },
+        building: { type: GraphQLString, description: "Building code found on https://www.reg.uci.edu/addl/campus/." },
+        room: { type: GraphQLString, description: "Room number in a building. Must also specify a building code." }
       },
 
       resolve: async (_, args) => {
