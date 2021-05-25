7a8d29f33fec985d487a3ff88af6713b67410879
ramanxg <ramanxg@gmail.com>
2021-05-24 18:49:53 -0700

    using circleci

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Dotenv is a zero-dependency module that loads environment
 // variables from a .env file into process.env
 require('dotenv').config();
-const serverless = require('serverless-http');
 
 var createError = require('http-errors');
+const serverless = require('serverless-http');
 const {createErrorJSON} = require("./helpers/errors.helper");
-var { apiKeyAuth } = require("./keys/apiKeyAuth");
+// var { apiKeyAuth } = require("./keys/apiKeyAuth");
 var express = require('express');
 var cors = require('cors');
 
@@ -22,38 +22,38 @@ var port = process.env.PORT || 8080;
 
 var restRouter = require('./rest/versionController');
 var graphQLRouter = require('./graphql/router');
-var generateKey = require('./keys/generateKey');
+// var generateKey = require('./keys/generateKey');
 
 var app = express();
 app.set('trust proxy', 1);
 
-const limiter = rateLimit({
-    windowMs: 60 * 1000, // 60 seconds * 1000ms
-    max: 100, // limit each IP to 100 requests per windowMs(minute)
-    message: createErrorJSON(429, "Too Many Requests", "You have exceeded the rate limit. Please try again later.")
-});
+// const limiter = rateLimit({
+//     windowMs: 60 * 1000, // 60 seconds * 1000ms
+//     max: 100, // limit each IP to 100 requests per windowMs(minute)
+//     message: createErrorJSON(429, "Too Many Requests", "You have exceeded the rate limit. Please try again later.")
+// });
 
-const moesifMiddleware = moesif({
-  applicationId: process.env.MOESIF_KEY,
+// const moesifMiddleware = moesif({
+//   applicationId: process.env.MOESIF_KEY,
 
-  // Link API Calls to Api Key
-  // getSessionToken: function (req, res) {
-  //   return req.headers["x-api-key"] ? req.headers["x-api-key"] : undefined;
-  // },
-});
+//   // Link API Calls to Api Key
+//   // getSessionToken: function (req, res) {
+//   //   return req.headers["x-api-key"] ? req.headers["x-api-key"] : undefined;
+//   // },
+// });
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Tracing.Integrations.Express({ app }),
-  ],
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: process.env.SENTRY_DSN,
+//   integrations: [
+//     // enable HTTP calls tracing
+//     new Sentry.Integrations.Http({ tracing: true }),
+//     // enable Express.js middleware tracing
+//     new Tracing.Integrations.Express({ app }),
+//   ],
+//   // We recommend adjusting this value in production, or using tracesSampler
+//   // for finer control
+//   tracesSampleRate: 1.0,
+// });
 
 var corsOptions = {
   origin: ['http://127.0.0.1:' + port, 'http://api.peterportal.org', 'https://api.peterportal.org'],
@@ -63,13 +63,13 @@ app.use(cors(corsOptions));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(limiter);
-app.use(moesifMiddleware);
+// app.use(limiter);
+// app.use(moesifMiddleware);
 // RequestHandler creates a separate execution context using domains, so that every
 // transaction/span/breadcrumb is attached to its own Hub instance
-app.use(Sentry.Handlers.requestHandler());
+// app.use(Sentry.Handlers.requestHandler());
 // TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
+// app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("./docs-site"));
@@ -107,4 +107,5 @@ app.use(function(err, req, res, next) {
 
 
 
+module.exports = app;
 module.exports.handler = serverless(app);
