54d1c95a2f0dbe1467356617b7f51f0a3554046b
Raman Gupta <ramanxg@gmail.com>
2021-07-01 15:06:20 -0700

    added manual sentry capture in error handler

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  Sentry.captureException(err);
   // render the error page
   var status = err.status || 500;
   res.status(status).send(createErrorJSON(status, err.message, ""));
