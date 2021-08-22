656cfe289df34ec96da7b7917fd5f2b7372ee4c7
Raman Gupta <ramanxg@gmail.com>
2021-08-22 09:53:27 -0700

    Some quick code fixes

--- utils/mapInstructorID.js
+++ utils/mapInstructorID.js
@@ -7,16 +7,17 @@ function mapInstructorName() {
     var name_to_ucinetid = {};
     // create a mapping from instructor shortened name to ucinetid
     instructors.map((data) => {
-        if (data["shortened_name"] in name_to_ucinetid) {
-            console.log("KEY EXISTS: ", data["shortened_name"] , data["name"], data["ucinetid"])
-            console.log("DUPLICATE: ", name_to_ucinetid[data["shortened_name"] ]);
-            name_to_ucinetid[data["shortened_name"] ] = name_to_ucinetid[data["shortened_name"] ].concat([data["ucinetid"]]);
+        const shortened_name = data["shortened_name"];
+        if (shortened_name in name_to_ucinetid) {
+            console.log("KEY EXISTS: ", shortened_name , data["name"], data["ucinetid"]);
+            console.log("DUPLICATE: ", name_to_ucinetid[shortened_name]);
+            name_to_ucinetid[shortened_name] = name_to_ucinetid[shortened_name ].concat([data["ucinetid"]]);
         } else {
-            name_to_ucinetid[data["shortened_name"]] = [data["ucinetid"]];
+            name_to_ucinetid[shortened_name] = [data["ucinetid"]];
         }
     });
     
-    console.log("Storing data...")
+    console.log("Storing data...");
     file_path = path.resolve('cache', 'instructor_name_map.json');
     storeData(name_to_ucinetid, file_path);
     console.log("Finished writing to file");
