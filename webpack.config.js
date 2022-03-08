4007388a2454a51facf2b014774d1e47027f5e10
Raman Gupta <ramanxg@gmail.com>
2022-03-08 12:30:07 -0800

    switched to webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  resolve: {
+    extensions: ['.ts', 'tsx']
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.serverless'),
+            path.resolve(__dirname, '.webpack'),
+          ],
+        ],
+      },
+    ],
+  },
+};
