73fcb749abcc1b0f12d85fcf0acda1955f45c1cb
Raman Gupta <ramanxg@gmail.com>
2022-04-05 21:02:23 -0700

    changed type of schedule to any

--- rest/v0/schedule.ts
+++ rest/v0/schedule.ts
@@ -6,7 +6,7 @@ import {getWeek} from '../../helpers/week.helper';
 import { WebsocResponse } from '../../types/websoc.types';
 
 router.get("/soc", function (req, res, next) {
-    callWebSocAPI(req.query).then((response: WebsocResponse) => {
+    callWebSocAPI(req.query).then((response: any) => {
         res.json(response)
     }).catch((err) => {
         res.status(400).json(createErrorJSON(400, "Bad Request: Invalid parameter", "Unable to complete websoc-api query"));
@@ -14,7 +14,7 @@ router.get("/soc", function (req, res, next) {
 })
 
 router.get("/week", function (req, res, next) {
-    getWeek(req.query.year, req.query.month, req.query.day).then((response: WebsocResponse) => {
+    getWeek(req.query.year, req.query.month, req.query.day).then((response: any) => {
         res.json(response)
     }).catch((err) => {
         res.status(400).json(createErrorJSON(400, "Bad Request: Invalid parameter", "Invalid year, month or day. Must include all year, month and day or none."));
