b743c3f8f1229aac49cdf68148228f864ecea3b6
Raman Gupta <ramanxg@gmail.com>
2022-05-16 00:38:12 -0700

    Added missing 'count' field to Grades

--- graphql/grades.ts
+++ graphql/grades.ts
@@ -31,7 +31,8 @@ const gradeDistributionCollectionAggregateType: GraphQLObjectType = new GraphQLO
     sum_grade_p_count: { type: GraphQLFloat }, 
     sum_grade_np_count: { type: GraphQLFloat }, 
     sum_grade_w_count: { type: GraphQLFloat }, 
-    average_gpa: { type: GraphQLFloat }
+    average_gpa: { type: GraphQLFloat },
+    count: {type: GraphQLFloat}
   })
 });
 
