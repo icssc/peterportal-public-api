13ea6fe6f357622fdf22798deb6171189c0c188a
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-09-11 22:36:15 -0700

    refactor: fix server.ts

--- server.ts
+++ server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const port = process.env.PORT || 8080;
 
 app.listen(port, function () {
-  console.log("Server is running on Port: " + port);
+  console.log(`Server is running on Port: ${port}`);
 });
